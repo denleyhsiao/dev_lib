3e17296e4dffd99bc513e4739cbd445509ce3ab4
Denley Hsiao <denley@justtodo.com>
2019-04-29 11:36:13 +0800

    Add deadline_time.

---
+++ include/dev_lib/deadline_time.h
@@ -0,0 +1,37 @@
+#ifndef __DEV_LIB_DEADLINE_TIME_H__
+#define __DEV_LIB_DEADLINE_TIME_H__
+
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+class DeadlineTime
+{
+public:
+  static bool isZero(const boost::posix_time::time_duration& duration);
+  DeadlineTime(float durationSeconds);
+  virtual ~DeadlineTime() {}
+  void init();
+  void destroy();
+  void cancel();
+  void countDown();
+
+protected:
+  static boost::posix_time::time_duration duration(float seconds);
+  bool isZero() const;
+  void reset(float durationSeconds);
+
+private:
+  virtual void doDestroy() = 0;
+  virtual void finishCountDown() = 0;
+  bool hasSyncCancel() const;
+  void callback(const boost::system::error_code &ec);
+  boost::asio::io_service io;
+  boost::asio::io_service::work work;
+  boost::posix_time::ptime end;
+  boost::posix_time::time_duration powerDuration;
+  boost::asio::deadline_timer powerTimer;
+  std::shared_ptr<boost::thread> thread;
+};
+
+#endif
---
+++ src/deadline_time.cpp
@@ -0,0 +1,64 @@
+#include "dev_lib/deadline_time.h"
+
+boost::posix_time::time_duration DeadlineTime::duration(float seconds)
+{
+  return boost::posix_time::milliseconds(seconds * 1000);
+}
+
+DeadlineTime::DeadlineTime(float durationSeconds) : work(io), end(boost::get_system_time()), powerTimer(io, powerDuration), thread(std::shared_ptr<boost::thread>(NULL))
+{
+  reset(durationSeconds);
+}
+
+void DeadlineTime::init()
+{
+  thread = std::shared_ptr<boost::thread>(new boost::thread(boost::bind(&boost::asio::io_service::run, &io)));
+}
+
+void DeadlineTime::reset(float durationSeconds)
+{
+  powerDuration = duration(durationSeconds);
+}
+
+void DeadlineTime::destroy()
+{
+  powerTimer.cancel();
+  doDestroy();
+  if (thread)
+    thread->join();
+}
+
+void DeadlineTime::cancel()
+{
+  end = boost::get_system_time();
+  powerTimer.cancel();
+}
+
+void DeadlineTime::countDown()
+{
+  end = boost::get_system_time();
+  powerTimer.expires_from_now(powerDuration);
+  powerTimer.async_wait(boost::bind(&DeadlineTime::callback, this, boost::asio::placeholders::error));
+}
+
+void DeadlineTime::callback(const boost::system::error_code &ec)
+{
+  if (ec || hasSyncCancel())
+    return ;
+  finishCountDown();
+}
+
+bool DeadlineTime::hasSyncCancel() const
+{
+  return isZero(boost::get_system_time() - end);
+}
+
+bool DeadlineTime::isZero() const
+{
+  return isZero(powerDuration);
+}
+
+bool DeadlineTime::isZero(const boost::posix_time::time_duration& duration)
+{
+  return (duration.total_milliseconds() == 0);
+}
