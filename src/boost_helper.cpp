82f21197cdd23e628d4acecc4f18941770799ed5
Denley Hsiao <denley@justtodo.com>
2019-05-08 14:12:39 +0800

    Add boost_helper test.

--- src/boost_helper.cpp
+++ src/boost_helper.cpp
@@ -6,14 +6,13 @@
 BoostHelper::floats_type BoostHelper::split(const char* value, const char delimiter /* = ',' */)
 {
   static const char SPACE_FLAG = ' ';
-  floats_type result = floats_type();
   std::string source = value;
   if (delimiter != SPACE_FLAG)
     std::replace(source.begin(), source.end(), delimiter, SPACE_FLAG);
 
   std::istringstream iss(source);
-  std::vector<std::string> values((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
-  for (std::string value: values)
-    result.push_back(boost::lexical_cast<float>(value));
+  floats_type result = floats_type();
+  std::for_each(std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>(),
+    [&result](const std::string& value){ result.push_back(boost::lexical_cast<float>(value)); });
   return result;
 }
---
+++ test/test_boost_helper.cpp
@@ -0,0 +1,21 @@
+#include "dev_lib/boost_helper.h"
+#include "dev_lib/dev_helper.h"
+#include <gtest/gtest.h>
+
+typedef BoostHelper::floats_type floats_type;
+
+TEST(BoostHelper, split)
+{
+  ASSERT_EQ(floats_type({1.2,3.4,5.6,7.8,9.0}), BoostHelper::split("1.2,3.4,5.6,7.8,9.0"));
+}
+
+TEST(BoostHelper, splitWithSpace)
+{
+  ASSERT_EQ(floats_type({1.2,3.4,5.6,7.8,9.0}), BoostHelper::split("1.2 3.4 5.6 7.8 9.0", ' '));
+}
+
+TEST(BoostHelper, splitWithInf)
+{
+  float inf = DevHelper::inf;
+  ASSERT_EQ(floats_type({1.2,3.4,inf,5.6,7.8,9.0,inf}), BoostHelper::split("1.2,3.4,inf,5.6,7.8,9.0,inf"));
+}
