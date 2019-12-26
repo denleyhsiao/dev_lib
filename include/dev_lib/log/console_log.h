78aaa4b1dce8256d3c5416d99d8f6c952b9d198e
Denley Hsiao <denley@justtodo.com>
2019-12-26 09:49:15 +0800

    Add console log create function.

--- include/dev_lib/log/console_log.h
+++ include/dev_lib/log/console_log.h
@@ -2,15 +2,20 @@
 #define __DEV_LIG_LOG_CONSOLE_LOG__
 
 #include "dev_lib/log/log.h"
+#include <memory>
 
 class ConsoleLog : public Log
 {
 public:
+  static std::shared_ptr<Log> create();
   virtual void debug(const std::string& message, const char* fileName = __FILE__, unsigned int line = __LINE__);
   virtual void info(const std::string& message, const char* fileName = __FILE__, unsigned int line = __LINE__);
   virtual void warn(const std::string& message, const char* fileName = __FILE__, unsigned int line = __LINE__);
   virtual void error(const std::string& message, const char* fileName = __FILE__, unsigned int line = __LINE__);
   virtual void fetal(const std::string& message, const char* fileName = __FILE__, unsigned int line = __LINE__);
+
+private:
+  ConsoleLog() { }
 };
 
 #endif
--- src/log/console_log.cpp
+++ src/log/console_log.cpp
@@ -1,6 +1,11 @@
 #include "dev_lib/log/console_log.h"
 #include <iostream>
 
+std::shared_ptr<Log> ConsoleLog::create()
+{
+  return std::shared_ptr<Log>(new ConsoleLog());
+}
+
 void ConsoleLog::debug(const std::string& message, const char* fileName /* = __FILE__ */, unsigned int line /* = __LINE__ */)
 {
   std::cout<<message<<std::endl;
